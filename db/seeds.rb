a98eea44fee01be1ecb199c7d9ab26dccd4045ee
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-05 00:09:30 +0900

    finished the index and show, re-did the seed and added a generic navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,24 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+
+puts 'cleaing database'
+Flat.destroy_all
+
+puts 'creating 10 flats'
+
 10.times do
-  Flat.create(
+  Flat.create!(
     name: Faker::JapaneseMedia::OnePiece.island,
-    address: "#{rand(1000..9999)} #{Faker::JapaneseMedia::OnePiece.location}",
+    address: "#{rand(1000..9999)} #{Faker::JapaneseMedia::OnePiece.location} ave.",
     description: Faker::JapaneseMedia::OnePiece.quote,
     price_per_night: rand(125..500),
     number_of_guest: rand(2..8)
   )
+end
+
+puts 'completed 10 flats'
+
+Flat.all.each_with_index do |flat, index|
+  puts "#{index + 1} - #{flat.name}"
 end
